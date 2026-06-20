8a19fadeaf445677c7ff872d4b086dd7de064baf
astishnaaroon1-design <astishnaaroon1@gmail.com>
2026-06-21 04:38:41 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import FileSidebar from '@/components/FileSidebar'
 import ChatPanel from '@/components/ChatPanel'
 
 export default function Home() {
   return (
     <main className="flex h-screen overflow-hidden">
-      <FileSidebar />
       <ChatPanel />
     </main>
   )
